c72a0a2971efd95beadbc32cdc665b5f872a440b
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2014-03-04 12:50:54 +0200

    Attempt to make the OCR more precise
    In my tests, the OCR has better times with PNG (probably due to
    compression). I did not manage to get a lossless format working so far,
    but should be a good thing to do in the future.
    
    Also, whitelisting the symbols (degrees, minutes, seconds), gives some
    hints to the parser and makes it more precise.

--- IBGETools/OCR.py
+++ IBGETools/OCR.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import re
 import tesseract
 
 from PythonMagick import Blob
@@ -6,19 +9,24 @@ from PythonMagick import Blob
 class OCR:
     def __init__(self):
         api = tesseract.TessBaseAPI()
-        api.Init(".","eng",tesseract.OEM_DEFAULT)
+        api.Init(".", "eng", tesseract.OEM_DEFAULT)
         api.SetPageSegMode(tesseract.PSM_AUTO)
-        api.SetVariable("tessedit_char_whitelist", "-0123456789")
+        api.SetVariable("tessedit_char_whitelist", "-0123456789°'\"")
+        api.SetVariable("chs_leading_punct", "-")
+        api.SetVariable("numeric_punctuation", "-°'\"")
 
         self._api = api
 
     def GetDecimalDegrees(self, image):
         blob = Blob()
-        image.magick("JPEG")
+        image.magick("PNG")
+        image.quality(100)
+
         image.write(blob)
 
         tesseract.ProcessPagesBuffer(blob.data, blob.length(), self._api)
-        coordinates = self._api.GetUTF8Text().split(' ')[:3]
+        text = self._api.GetUTF8Text().replace(' ', '')
+        coordinates = re.split("°|'|\"| ", text)[:3]
 
         return self._ConvertToDecimalDegrees(coordinates)
 
@@ -27,8 +35,10 @@ class OCR:
         minutes = float(coordinates[1]) / 60
         seconds = float(coordinates[2]) / 3600
 
-        if (degrees < 0):
-            minutes *= -1
-            seconds *= -1
+        if (degrees > 0):
+            degrees *= -1
+
+        minutes *= -1
+        seconds *= -1
 
         return degrees + minutes + seconds

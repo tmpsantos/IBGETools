ae4e2c9f15096a39e7df98b198150cd670d7eab8
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2014-03-16 16:54:19 +0200

    Generate the KML using Jinja templates
    More readable. At the beginning I decided not to use Jinja because
    our use case was too simple and sounded overkill, but now it will
    be needed for the TileMill project files.

--- IBGETools/Utils.py
+++ IBGETools/Utils.py
@@ -1,34 +1,33 @@
 import os
 import stat
 
+from jinja2 import Environment, FileSystemLoader
+
+
+def _GetJinjaTemplate(name):
+    this_directory = os.path.dirname(os.path.realpath(__file__))
+    templates_directory = os.path.join(this_directory, "templates")
+    environment = Environment(loader=FileSystemLoader(templates_directory))
+
+    return environment.get_template(name)
+
+
 def KMLFileWriter(kml, id, region):
-    kml.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-    kml.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
-    kml.write("  <Folder>\n")
+    def ItemList():
+        for ibge_map in region.GetRectangles():
+            basename = os.path.splitext(os.path.basename(ibge_map.GetPath()))[0]
+            href = "%s_%s.png" % (id, basename)
 
-    for ibge_map in region.GetRectangles():
-        north = "{0:.10f}".format(ibge_map.GetY())
-        south = "{0:.10f}".format(ibge_map.GetY() - ibge_map.GetHeight())
-        east = "{0:.10f}".format(ibge_map.GetX() + ibge_map.GetWidth())
-        west = "{0:.10f}".format(ibge_map.GetX())
+            item = {"href": href,
+                    "north": ibge_map.GetTop(),
+                    "south": ibge_map.GetBottom(),
+                    "east": ibge_map.GetRight(),
+                    "west": ibge_map.GetLeft()}
 
-        basename = os.path.splitext(os.path.basename(ibge_map.GetPath()))[0]
+            yield item
 
-        kml.write("    <GroundOverlay>\n")
-        kml.write("      <Icon>\n")
-        kml.write("        <href>" + "%s_%s.png" % (id, basename) + "</href>\n")
-        kml.write("      </Icon>\n")
-        kml.write("      <LatLonBox>\n")
-        kml.write("        <north>" + north + "</north>\n")
-        kml.write("        <south>" + south + "</south>\n")
-        kml.write("        <east>" + east + "</east>\n")
-        kml.write("        <west>" + west + "</west>\n")
-        kml.write("      </LatLonBox>\n")
-        kml.write("    </GroundOverlay>\n")
-
-    kml.write("  </Folder>\n")
-    kml.write("</kml>\n")
-
+    template = _GetJinjaTemplate("kml.jinja")
+    kml.write(template.render(item_list=ItemList()))
 
 
 def TileScriptFileWriter(script, id, region):

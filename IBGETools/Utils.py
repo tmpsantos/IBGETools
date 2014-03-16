ba9b4ccd31ade567012f675b8bcfe6a3d1bb5bf0
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2014-03-16 18:10:29 +0200

    Generate .mbtiles!
    Yeah, now --tiles generates .mbtiles using TileMill which is way
    faster then gda2tiles.py. The generated file is ready to be uploaded
    as a Mapbox layer or can be used in a self-hosted tile server.

--- IBGETools/Utils.py
+++ IBGETools/Utils.py
@@ -31,55 +31,28 @@ def KMLFileWriter(kml, id, region):
 
 
 def TileScriptFileWriter(script, id, region):
-    # Shell script standard header
-    script.write("#!/bin/sh\n\n")
+    def ItemList():
+        for ibge_map in region.GetRectangles():
+            basename = os.path.splitext(os.path.basename(ibge_map.GetPath()))[0]
+            name = "%s_%s" % (id, basename)
 
-    translate_options = "-of vrt"
-    # Wild guess taken from here:
-    # http://wiki.osgeo.org/wiki/Brazilian_Coordinate_Reference_Systems
-    source_projection = str(
-            "+datum=WGS84 +proj=latlong +ellps=GRS80 +towgs84=0,0,0 +no_defs")
+            item = {"name": name,
+                    "north": ibge_map.GetTop(),
+                    "south": ibge_map.GetBottom(),
+                    "east": ibge_map.GetRight(),
+                    "west": ibge_map.GetLeft()}
 
-    current_path = os.getcwd()
+            yield item
 
-    for ibge_map in region.GetRectangles():
-        north = "{0:.10f}".format(ibge_map.GetY())
-        south = "{0:.10f}".format(ibge_map.GetY() - ibge_map.GetHeight())
-        east = "{0:.10f}".format(ibge_map.GetX() + ibge_map.GetWidth())
-        west = "{0:.10f}".format(ibge_map.GetX())
+    data = {"id": id,
+            "current_path": os.getcwd(),
+            "north": region.GetTop(),
+            "south": region.GetBottom(),
+            "east": region.GetRight(),
+            "west": region.GetLeft()}
 
-        coordinates = "%s %s %s %s" % (west, north, east, south)
-
-        # Use absolute paths on the .vrt files so tools like TileMill can
-        # open it. GDAL doesn't have this limitation.
-        basename = os.path.splitext(os.path.basename(ibge_map.GetPath()))[0]
-        name = os.path.join(current_path, "%s_%s" % (id, basename))
-
-        script.write(
-                "gdal_translate %s -a_srs '%s' -a_ullr %s %s.tif %s.vrt\n" %
-                (translate_options, source_projection, coordinates, name, name))
-
-    merge_options = "-resolution highest"
-    script.write(
-            "gdalbuildvrt %s -o %s.vrt %s_*.vrt\n" %
-            (merge_options, id, os.path.join(current_path, id)))
-
-    # TODO: Create the project file
-
-    north = "{0:.10f}".format(region.GetTop())
-    south = "{0:.10f}".format(region.GetBottom())
-    east = "{0:.10f}".format(region.GetRight())
-    west = "{0:.10f}".format(region.GetLeft())
-
-    bbox = "%s,%s,%s,%s" % (west, south, east, north)
-
-    tiles_options = str(
-            "export %s %s.mbtiles --format=mbtiles --metatile=16" % (id, id))
-    tiles_zoom = "--minzoom=8 --maxzoom=19"
-
-    script.write(
-            "node /usr/share/tilemill/index.js %s %s --bbox=%s\n" %
-            (tiles_options, tiles_zoom, bbox))
+    template = _GetJinjaTemplate("script.jinja")
+    script.write(template.render(item_list=ItemList(), data=data))
 
     script_stat = os.fstat(script.fileno())
     os.fchmod(script.fileno(), script_stat.st_mode | stat.S_IEXEC)
